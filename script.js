5da90debe179cbe256537ff26e64b6d3f2fa2518
Seergei Pozdeev <Pozdeev.2002@inbox.ru>
2022-07-05 22:32:09 +0500

    try localstorage and layout(console errors? maybe is it problem?)

--- script.js
+++ script.js
@@ -9,26 +9,26 @@ const btnAddNode = document.querySelector('.modal__btn2');
 const modalForm = document.querySelector('.modal__form');
 
 //Массив в который будем кидать объект
-let notes = [];
+const notes = []
 
-btnOpenModel.onclick = () =>  {
+btnOpenModel.onclick = () => {
     // отобразим модальное окно
     modal.classList.remove('hide');
-  };
+};
 
 btnCloseModal.onclick = () => {
     modal.classList.add('hide');
 }
-modalForm.onsubmit = (e) =>{
+modalForm.onsubmit = (e) => {
     //В эту ф-ию будет передано событие ивент, мы не хотим чтобы страница перезагружалась => event есть метод e.preventDefault(); Убираем стандартное поведение формы
     console.log(e);
     e.preventDefault();
 
-    
+
 }
 
-window.onclick = function(event){
-    if(event.target == modal){
+window.onclick = function (event) {
+    if (event.target == modal) {
         modal.classList.add('hide');
     }
 }
@@ -39,33 +39,38 @@ btnAddNode.onclick = () => {
 
     //Каждое новое дело будем записывать в объект, а уже объект добавлять в массив
 
-    const note = {
+    let note = {
         title: modalTitle.value,
         content: modalDescr.value,
         imageLink: modalImagesLink.value,
         createDate:
-        String(new Date().getDate()).padStart(2, 0) +
-        "." +
-        String(new Date().getMonth() + 1).padStart(2, 0) +
-        "." +
-        new Date().getFullYear(),
+            String(new Date().getDate()).padStart(2, 0) +
+            "." +
+            String(new Date().getMonth() + 1).padStart(2, 0) +
+            "." +
+            new Date().getFullYear(),
         lastModified: null,
         modified: false,
     };
-    
+    localStorage.setItem('note', JSON.stringify(note));
+    note = JSON.parse(localStorage.getItem('note'));
+    console.log(note);
 
 
-    
-    if(note.title != '' && note.content != '' ){
+
+    if (note.title != '' && note.content != '') {
         modal.classList.add('hide');
         notes.push(note);
-        console.log(notes);
         modalTitle.value = '';
         modalImagesLink.value = '';
         modalDescr.value = '';
         insertNotes(notes);
+        //!Проверка условия 
+        console.log(notes);
     }
-    
+    //!localstorage устанавливаем значение
+    // localStorage.setItem('notes', JSON.stringfy(notes));
+
 
 
     //Добавляем в туду лист наш объект
@@ -75,35 +80,38 @@ btnAddNode.onclick = () => {
 };
 
 
-
+//Поиск
 function findNotesByWord(word) {
     return notes.filter(
         ({ title, content, createDate }) =>
             content.includes(word) || title.includes(word) || createDate === word
     );
 }
+
 document.querySelector("#finderInput").addEventListener("input", (e) => {
     document.querySelector(".notes").innerHTML = createNotes(
         findNotesByWord(e.target.value)
     ).join("");
 });
 
-function insertNotes(notes){
+function insertNotes(notes) {
     document.querySelector(".notes").innerHTML = createNotes(notes).join("");
 }
 
 function createNotes(notes) {
     return notes.map(({ title, content, createDate, modified, lastModified, imageLink }) => {
         return `
+        
             <div class="note">
-                    <h2>${title}</h2>
-                    <div>${content}</div>
-              <p>Создано: <i>${createDate}</i></p>
-              ${imageLink ? `<img class="images__js" src="${imageLink}" alt="">`: ''}
-              ${modified ? `<p>Изменено: <i> ${lastModified}</i></p>`: ''}
+                    <h2 class="note__title">${title}</h2>
+                    <div class="note__content">${content}</div>
+                    <p class="note__createDate">Создано: ${createDate}</p>
+                    ${imageLink ? `<img class="images__js" src="${imageLink}" alt="">` : ''}
+                    ${modified ? `<p>Изменено: <i> ${lastModified}</i></p>` : ''}
             </div>
         `;
     });
 }
 
 insertNotes(notes);
+// console.log(notes);
